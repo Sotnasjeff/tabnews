0f03d50d6fce3fa1260628ee2f88f7107800abb4
Jefferson Santos <jeffdeutsch1@gmail.com>
2025-12-16 00:09:29 -0300

    add scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Maravilha, agora vai!</h1>
+  return <h1>Maravilha, agora vai!</h1>;
+}
+
+function Test() {
+  return "";
+}
+
+function teste2() {
+  return "";
 }
 
 export default Home;
