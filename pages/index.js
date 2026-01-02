b89e94ad6708097a6ff28d531b18cc13aad7d031
Jefferson Santos <jeffdeutsch1@gmail.com>
2026-01-02 14:42:28 -0300

    fix: just a commit to create a new domain in vercel

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 function Home() {
-  return <h1>Maravilha, agora vai!</h1>;
+  return <h1>Maravilha, agora vai, vamos alterar isso!</h1>;
 }
 
 function Test() {
-  return "";
-}
-
-function teste2() {
   return "";
 }
 
