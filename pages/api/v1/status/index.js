46071b34dbab508c16e604b8cbde2c714368ebfe
Jefferson Santos <jeffdeutsch1@gmail.com>
2026-01-20 01:07:54 -0300

    finish endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,27 @@
-import database from "cursoDevInfra/database";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
+  const postgresVersion = await database.query("SHOW server_version;");
+  let databaseVersion = postgresVersion.rows[0].server_version;
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  let maxConnections = databaseMaxConnections.rows[0].max_connections;
+
+  const databaseName = "local_db";
+  const databaseOpenedConnection = await database.query({
+    text: `SELECT count(*) FROM pg_stat_activity WHERE datname = $1 AND state = 'active';`,
+    values: [databaseName],
+  });
+
+  let openedConnections = databaseOpenedConnection.rows[0].count;
+  const updatedAt = new Date().toISOString();
   response.status(200).json({
-    mensagem: "Exito",
+    updated_at: updatedAt,
+    dependencies: {
+      database_version: databaseVersion,
+      max_connections: Number(maxConnections),
+      opened_connections: Number(openedConnections),
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+  expect(responseBody.dependencies.database_version).toBe("16.0");
+  expect(responseBody.dependencies.max_connections).toBe(100);
+  expect(responseBody.dependencies.opened_connections).toBe(1);
 });
